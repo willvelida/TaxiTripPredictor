e817374b49f1b8d301748175a1b35bc56647a1cb
Will Velida <willvelida@hotmail.co.uk>
2020-04-09 18:51:10 +1200

    Adding taxi trip models

---
+++ TaxiTripPredictor/TaxiTripPredictor.Common/Models/TaxiTripFarePrediction.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaxiTripPredictor.Common.Models
+{
+    public class TaxiTripFarePrediction
+    {
+        [ColumnName("Score")]
+        public float FareAmount;
+    }
+}
